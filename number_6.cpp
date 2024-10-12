d6859e7c31c86948e0994c6db041ae726ffdac4a
Timofey Kanunnikov <135886248+soki-rf@users.noreply.github.com>
2024-10-12 17:29:07 +0300

    Create number_6.cpp

---
+++ number_6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+    int i = 0;
+    int cnt1 = 0;
+    int cnt2 = 0;
+
+    while(s1[i++] != '\0'){
+        cnt1++;
+    }
+
+    while(s2[i++] != '\0'){
+        cnt2++;
+    }
+
+    if(cnt1 > cnt2){
+        cout << "first";
+    }else{
+        if (cnt1 < cnt2){
+            cout << "second";
+        }else{
+            cout << "same";
+        }
+
+    }
+    return 0;
+}
